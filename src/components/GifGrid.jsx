b9b127599ce0f5600ab32299bccbad3065f16fb9
ChristianR <dlless@gmail.com>
2023-04-11 20:24:16 -0500

    * Separate functions in Hooks

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,24 +1,17 @@
-import { useEffect, useState } from "react";
-
-import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    const getImages = async () => {
-        const newImages = await getGifs( category );
-        setImages( newImages );
-    }
-
-    useEffect( () => {
-        getImages(category);        
-    }, [  ])
+    const { images, isLoading } = useFetchGifs( category );
+    console.log(isLoading);
 
     return (
         <>
             <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
             <div className="card-grid">
                 {
                     images.map( ( image ) => (
